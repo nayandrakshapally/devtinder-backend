dbcaa2e682b545d2bb297d7baf05ea9d2a99e1b9
nayandrakshapally <nayan.drakshapally@gmail.com>
2024-10-16 21:50:00 -0400

    Error handling

--- src/app.js
+++ src/app.js
@@ -18,7 +18,20 @@ app.get("/user", userAuth, (req, res) => {
 });
 
 app.get("/user/login", (req, res) => {
-  res.send("Here we go user login!");
+  //try catch implementation is must for each route to handle errors
+  try {
+    throw new Error("big error!");
+    res.send("Here we go user login!");
+  } catch (err) {
+    res.status(400).send("error in this login api");
+  }
+});
+
+// Global error handling is must and should keep this at the end of all the routes
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
 });
 
 app.listen(4000, () => {
